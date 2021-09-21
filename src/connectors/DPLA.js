0e8d55cd96b3fac629af0b41f10d3454a127c401
Rainer Simon <aboutgeo@gmail.com>
2021-09-21 17:22:01 +0200

    DPLA result parsing bugfix

--- src/connectors/DPLA.js
+++ src/connectors/DPLA.js
@@ -12,24 +12,23 @@ export default class DPLA {
   query(query) {
     return fetch(`/dpla?q=${query}&api_key=${this.config.apiKey}`)
       .then(response => response.json())
-      .then(data => { 
-        console.log(data);
-        data.docs.map(doc => {
-          const uri = doc.isShownAt;
-          const label = doc.sourceResource.title[0];
-          const description = doc.sourceResource.creator[0];
-          return {
-            uri, label, description
-          }
-        })
-      }
+      .then(data => data.docs.map(doc => {
+        const uri = doc.isShownAt;
+        const label = doc.sourceResource.title[0];
+        const description = doc.sourceResource.creator && doc.sourceResource.creator[0];
+
+        return {
+          uri, label, description, 
+          dpla: true // So we can identify DPLA suggestions
+        }
+      })
     );
   }
 
 }
 
 /** DPLA provides original URIs - need to think about proper formatting **/
-DPLA.matches = tag => false;
+DPLA.matches = tag => tag.dpla;
 
 /** DPLA provides original URIs - need to think about proper formatting **/
-DPLA.format = tag => tag.uri;
+DPLA.format = tag => tag.uri.substring(tag.uri.lastIndexOf('/') + 1);
